82b83733b8cc3676e68f830e8680d9e27115b66c
Esraa Dhair <esraadhair98@gmail.com>
2020-07-01 11:51:48 +0300

    Special Training

--- app/Http/Controllers/controlpanelcontrollers/TrainingController.php
+++ app/Http/Controllers/controlpanelcontrollers/TrainingController.php
@@ -1,16 +1,39 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\controlpanelcontrollers;
 
-use App\Training;
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+const STUDENT_PAGINATION = 10;
 
 class TrainingController extends Controller
 {
     public function getSpecialTrainingStudents(){
-//        $sTraining=Training::
-
+        $students = DB::table('users')
+        ->join('training','users.id','=','training.studentId')
+        ->where('type','=','S')
+        ->select('users.*','training.*');
+        return view('base_layout.training.specialTraining',['students'=>$students]);
     }
+
+    public function getApproved(Request $request){
+        $students = DB::table('users')
+            ->join('training','users.id','=','training.studentId')
+            ->where('type','=','S')
+            ->select('users.*','training.*')
+           ->get();
+        foreach ($students as $student){
+            if ($request->has('chkBox')) {
+                $student->approved= 1;
+                DB::table('users')
+                    ->join('training','users.id','=','training.studentId')
+                    ->update(['approved' => 1]);
+            }
+        }
+        return view('base_layout.training.specialTraining',['students'=>$students]);
+    }
+
     public function getGeneralTrainingStudents(){
 
     }
